c9e62efbd9e1fe96ca97f3e20ad3c1ebe05e5444
Heloísa Meister <89104167+helomeister@users.noreply.github.com>
2022-03-19 19:27:58 -0300

    atualização 19/03 - helo e schelen

---
+++ checkpoint1/checkpoint1/script.js
@@ -0,0 +1,49 @@
+/* if usuário apertar submit , ciar uma variavel 
+let novoCard = document.createElement 
+console log (novoCard);
+
+ciar no html estrutura do card
+*/
+
+//Criando uma função para capturar info dos usuários 
+function salvarInfo() {
+let botao = document.querySelector('button')
+
+botao.addEventListener('click', updateButton);
+
+if(button.value === 'Star machine'){
+    button.value = 'Stop machine';
+    let assunto =    
+}
+
+}; 
+
+//Criando um novo Card
+let novoCard = document.createElement('div');
+console.log(div);
+
+novoCard.innerText = "Novo Card";
+console.log(div);
+
+//Adicionando nova entrada
+let tituloCard = document.getElementById('entrada1');
+console.log(entrada1);
+
+
+//tem que criar uma variável para cada entrada do formulário e utilizá-la para exibir no card
+novoCard.innerHTML = `
+<div class="card"> 
+    <h2>tituloCard</h2>
+    <img src="${entrada2}">
+     <p>
+     TextoInserido
+     </p>
+ </div>`;
+
+//Quando for necessário exibir o card inteiro
+ novoCard.appendChild(card1);
+
+ let inserirImg = document.querySelectorAll('img');
+ let novoCard = document.querySelectorAll('h2');
+ //SetAtribute= Adiciona um novo atributo ou modifica o valor 
+ let link = inserirImg.setAtribute("src", link);
