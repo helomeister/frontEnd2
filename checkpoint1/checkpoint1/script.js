775a4237e5ca96a5d34d9686f94407eeb3beb2e3
Heloísa Meister <89104167+helomeister@users.noreply.github.com>
2022-03-20 15:07:35 -0300

    tentativa de criação de card

--- checkpoint1/checkpoint1/script.js
+++ checkpoint1/checkpoint1/script.js
@@ -7,33 +7,46 @@ ciar no html estrutura do card
 
 //Criando uma função para capturar info dos usuários 
 let botao = document.querySelector("#submit");
+let newcard = document.querySelector('.card');
+let container1 = document.querySelector('.container1')
+let titulo;
+let img;
+let txt;
+
 botao.addEventListener("click",function(e){
     e.preventDefault();
 
-    const tituloCard = document.querySelector("#entrada1");
-    const imagem = document.querySelector("#entrada2");
-    const texto = document.querySelector("#entrada3");
+    let tituloCard = document.querySelector("#entrada1");
+    let imagem = document.querySelector("#entrada2");
+    let texto = document.querySelector("#entrada3");
    
-    let titulo = tituloCard.value;
-    let img = imagem.value;
-    let txt = texto.value;
+    titulo = tituloCard.value;
+    img = imagem.value;
+    txt = texto.value;
 
     console.log(titulo, img, txt);
     
 });
 
-//Criando um novo Card
-//let novoCard = document.createElement('div');
+//Criando um novo Card// não funcionooouu!
+function createCard(){
+    if (txt != null )
 
+    let novoCard = document.createElement('div')
+    novoCard.innerHTML = `
+        <div class="container1"> 
+            <h2>${titulo}</h2>
+            <img src="${img}">
+            <p>
+            "${txt}"
+            </p>
+        </div>`;
+}
 
-//novoCard.innerHTML = `
-//<div class="card"> 
-    //<h2>titulo</h2>
-    //<img src="img">
-    // <p>
-    // txt
-    // </p>
- //</div>`;
+container1.insertBefore(novoCard, container1.firstChild);    
+//container1.lastElementChild.remove();
+//container1.lastElementChild.remove();
+
 
 //Quando for necessário exibir o card inteiro
  //novoCard.appendChild(card1);
